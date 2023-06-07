a9de5bf852b10c4094d86d4f26f7039e9c7f8daa
Tamar Weiss <tamarweiss1292014@gmail.com>
2023-06-07 20:11:43 +0300

    implemented Snake calls into the game instead of the old variable.

--- snake.py
+++ snake.py
@@ -1,10 +1,10 @@
 from typing import Optional
 
-from consts import GROW_BONUS, SNAKE_SIZE, UP, inverse_directions
+from consts import GROW_BONUS, Point, SNAKE_SIZE, UP, inverse_directions
 from wall import get_next_pos
 
 class Snake:
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: int, y: int, debug=False):
         self.__facing = UP
         self.__grow_counter = 0
         self.collided = False
@@ -12,10 +12,11 @@ class Snake:
             (x, y - i)
             for i in range(SNAKE_SIZE)
             if y - i >= 0
-        ]
+        ] if not debug else []
 
     def __count_down(self):
-        if self.__grow_counter > 0:  # ticking down the counter one at a time
+        # ticking down the counter one at a time
+        if self.__grow_counter > 0:
             self.__grow_counter -= 1
 
     def turn(self, direction: Optional[str]):
@@ -23,20 +24,33 @@ class Snake:
         if direction and direction != inverse_directions[self.__facing]:
             self.__facing = direction
 
-    def move(self):
-        coordinates = self.coordinates
-        pos = get_next_pos(coordinates[0], self.__facing)
-        # check if the snake has crossed itself or the boundaries
+    def get_next_pos(self) -> Point:
+        return get_next_pos(self[0], self.__facing)
+
+    def move(self, pos: Point):
+        # check if the snake has crossed itself
         if pos in self.coordinates:
             self.flag_collision()
-        else:
-            coordinates.insert(0, pos)  # add the new position as the snake's head
+        elif not self.collided:  # if it hasn't collided with somthing already
+            self.coordinates.insert(0, pos)  # add the new position as the snake's head
 
         # the snake will grow as long as grow_counter is bigger than 0
+        if not self.__grow_counter or self.collided:
+            self.coordinates.pop()
         self.__count_down()
 
     def grow(self):
-        self.__grow_counter += GROW_BONUS + int(not self.__grow_counter)
+        self.__grow_counter += GROW_BONUS
+        return int(len(self) ** 0.5)
 
     def flag_collision(self):
         self.collided = True
+
+    def __len__(self) -> int:
+        return len(self.coordinates)
+
+    def __getitem__(self, index: int) -> Point:
+        return self.coordinates[index]
+
+    def __add__(self, other: list[Point]) -> list[Point]:
+        return self.coordinates + other
--- snake_game.py
+++ snake_game.py
@@ -3,41 +3,30 @@ import argparse
 from consts import *
 from game_display import GameDisplay
 from game_utils import get_random_apple_data, get_random_wall_data
-from wall import Wall, get_next_pos
+from snake import Snake
+from wall import Wall
 
 # noinspection PyProtectedMember
 class SnakeGame:
     # ------------------------------------------------------------------
-    # init methods
+    # init method
     # ------------------------------------------------------------------
 
     def __init__(self, gd: GameDisplay, args: argparse.Namespace):
         self.__gd = gd
         self.__rounds: int = args.rounds
         self.__out_of_bounds = False
-        self.__facing = UP
         self.__score = 0
-        self.__grow_counter = 0
         self.__debug: bool = args.debug
         self.__max_apples: int = args.apples
         self.__max_walls: int = args.walls
         self.__gd.show_score(self.__score)
 
         # game's objects
-
-        self.__snake = self.__init_snake()  # TODO: convert this into class
+        # the snake starts in the middle of the screen
+        self.__snake = Snake(self.__gd.width // 2, self.__gd.height // 2, args.debug)
         self.__apples: list[Point] = []
         self.__walls: list[Wall] = []
-
-    def __init_snake(self) -> list[Point]:
-        if not self.__debug:
-            x, y = self.__gd.width // 2, self.__gd.height // 2  # the snake starts in the middle of the screen
-            return [
-                (x, y - i)
-                for i in range(SNAKE_SIZE)
-                if y - i >= 0
-            ]
-        return []
 
     # ------------------------------------------------------------------
     # private methods
@@ -76,20 +65,14 @@ class SnakeGame:
 
     # TODO: add collision detection for walls
     def __move_snake(self):
-        pos = get_next_pos(self.__snake[0], self.__facing)
-        # check if the snake has crossed itself or the boundaries
-        if not self.__check_inbounds(pos) or pos in self.__snake:
-            self.__out_of_bounds = True
-        else:
-            self.__snake.insert(0, pos)  # add the new position as the snake's head
-
-        # the snake will grow as long as grow_counter is bigger than 0
-        if self.__grow_counter <= 0 or self.__out_of_bounds:
-            self.__snake.pop()
+        pos = self.__snake.get_next_pos()
+        # check if the snake has crossed the boundaries
+        if not self.__check_inbounds(pos):
+            self.__snake.flag_collision()
+        self.__snake.move(pos)
 
     def __eat_apple(self):
-        self.__update_score(int(len(self.__snake) ** 0.5))
-        self.__grow_counter += GROW_BONUS + int(not self.__grow_counter)
+        self.__update_score(self.__snake.grow())
         self.__apples.remove(self.__snake[0])
 
     def __update_score(self, score: int):
@@ -115,9 +98,7 @@ class SnakeGame:
 
     def read_key(self):
         key_clicked = self.__gd.get_key_clicked()
-        # if we input a direction which is not the inverse to our current one
-        if key_clicked and key_clicked != inverse_directions[self.__facing]:
-            self.__facing = key_clicked
+        self.__snake.turn(key_clicked)
 
     def update_objects(self):
         # move walls every even round
@@ -127,6 +108,7 @@ class SnakeGame:
 
         # activate the snake-related functions (as long as it's not debug mode)
         if not self.__debug:
+            self.__move_snake()
             if self.__snake[0] in self.__apples:
                 self.__eat_apple()
 
@@ -136,18 +118,17 @@ class SnakeGame:
 
     def draw_board(self):
         self.__draw_objects(self.__apples, APPLE_COLOR)  # drawing the apples
-        not self.__debug and self.__draw_objects(self.__snake, SNAKE_COLOR)  # drawing the snake (unless its debug mode)
+        if not self.__debug:  # drawing the snake (unless its debug mode)
+            self.__draw_objects(self.__snake.coordinates, SNAKE_COLOR)
         for wall in self.__walls:  # lastly, drawing the walls
             self.__draw_objects(self.__get_wall_coordinates(wall), WALL_COLOR)
 
     def end_round(self):
         self.__gd.end_round()  # responsible for updating the game screen
-        if self.__grow_counter > 0:  # ticking down the counter at the end of each round
-            self.__grow_counter -= 1
 
     def is_over(self) -> bool:
         rounds_over = self.__gd._round_num == self.__rounds
-        return rounds_over or self.__out_of_bounds
+        return rounds_over or self.__snake.collided
 
     def game_over(self):
         self.__gd.show_score(f'{self.__score}. Game Over!')
