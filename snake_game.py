b0cb1afdd1f0a7aff4f8b6e9a9b0b38542c6c8d5
roberto <roberto.jubran@mail.hhuji.ac.il>
2023-06-07 14:01:59 +0300

    updated game in debug mode

--- snake_game.py
+++ snake_game.py
@@ -1,19 +1,22 @@
 import argparse
 
-from consts import *
 import game_utils
+from consts import *
 from game_display import GameDisplay
 
-Location = tuple[int, int]
+Point = tuple[int, int]
+
 
 def check_inbounds(num: int, length: int) -> bool:
     return 0 <= num < length
 
-def get_next_pos(head: Location, direction: str) -> Location:
+
+def get_next_pos(head: Point, direction: str) -> Point:
     x, y = head
     x += -int(direction == LEFT) + int(direction == RIGHT)
     y += -int(direction == DOWN) + int(direction == UP)
     return x, y
+
 
 class SnakeGame:
     # ------------------------------------------------------------------
@@ -33,40 +36,38 @@ class SnakeGame:
         self.__gd.show_score(self.__score)
 
         # game's objects
+
+        self.__snake = self.__init_snake()
+        self.__apples: list[Point] = []
+        self.__walls: list[list[Point]] = []
+
+        self.add_apples()
+
+    def __init_snake(self) -> list[Point]:
         if not self.__debug:
-            self.__snake = self.__init_snake()
-        self.__apples: list[Location] = []
-        self.__walls: list[list[Location]] = []
-
-    def __init_snake(self) -> list[Location]:
-        x, y = self.__gd.width // 2, self.__gd.height // 2  # the snake starts in the middle of the screen
-        return [
-            (x, y - i)
-            for i in range(SNAKE_SIZE)
-            if y - i >= 0
-        ]
-    # TODO check collision with walls
-    def __init_apples(self):
-        """this method initializes the apples"""
-        x, y = game_utils.get_random_apple_data()
-        if (x, y) not in self.__snake:
-            self.__apples.append((x,y))
-
-    # TODO check collision with walls
-    # TODO does this include the i + 1 round if game is over?
-    def add_apples(self):
-        x, y = game_utils.get_random_apple_data()
-        while len(self.__apples) < self.__max_apples:
-            if (x, y) not in self.__snake and (x, y) not in self.__snake and self.is_over():
-                self.__apples.append((x, y))
-
+            x, y = self.__gd.width // 2, self.__gd.height // 2  # the snake starts in the middle of the screen
+            return [
+                (x, y - i)
+                for i in range(SNAKE_SIZE)
+                if y - i >= 0
+            ]
+        return []
 
     # ------------------------------------------------------------------
     # private methods
     # ------------------------------------------------------------------
 
+    # TODO check collision with walls
+    # TODO does this include the i + 1 round if game is over?
+    def add_apples(self):
+        if len(self.__apples) < self.__max_apples:
+            pos = game_utils.get_random_apple_data()
+            occupied_cells = self.__snake + self.__apples
+            if pos not in occupied_cells:
+                self.__apples.append(pos)
+
     # TODO: add collision detection for walls
-    def __move_snake(self, pos: Location) -> None:
+    def __move_snake(self, pos: Point) -> None:
         width, height = self.__gd.width, self.__gd.height
         is_OOB = not check_inbounds(pos[0], width) or not check_inbounds(pos[1], height)
         # check if the snake has crossed itself or the boundaries
@@ -83,15 +84,13 @@ class SnakeGame:
     def __eat_apple(self) -> None:
         self.__update_score(int(len(self.__snake) ** 0.5))
         self.__grow_counter += GROW_BONUS
-        for apple in self.__apples:
-            if self.__snake[0] == apple:
-                self.__apples.remove(apple)
+        self.__apples.remove(self.__snake[0])
 
     def __update_score(self, score: int) -> None:
         self.__score += score
         self.__gd.show_score(self.__score)
 
-    def __draw_items(self, cells: list[Location], color: str) -> None:
+    def __draw_items(self, cells: list[Point], color: str) -> None:
         for x, y in cells:
             self.__gd.draw_cell(x, y, color)
 
@@ -107,9 +106,12 @@ class SnakeGame:
 
     # TODO: support other objects
     def update_objects(self) -> None:
-        if not self.__debug:  # updates the snake object (as long as it's not debug mode)
+        if not self.__debug:  # activate the snake-related functions (as long as it's not debug mode)
             pos = get_next_pos(self.__snake[0], self.__facing)
             self.__move_snake(pos)
+            if self.__snake[0] in self.__apples:
+                self.__eat_apple()
+        self.add_apples()
 
     def draw_board(self) -> None:
         self.__draw_items(self.__apples, APPLE_COLOR)  # drawing the apples
